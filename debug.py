5124988d869ffc83b209b9f0ad3d849269633854
guusrongen <guusrongen@gmail.com>
2023-11-05 21:52:38 +1300

    Minor edits debug scripts
    Minor edits debug scripts

--- debug.py
+++ debug.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     # Import GUI
     from anduryl.core.main import Project
     from anduryl.ui.main import MainWindow
-    from anduryl.io.settings import CalculationSettings, CalibrationMethod
+    from anduryl.io.settings import CalculationSettings, CalibrationMethod, Distribution
 
     # Create main window
     ex = MainWindow(app)
@@ -32,14 +32,41 @@ if __name__ == "__main__":
     # projectfile = r"d:\TUD\P03 - Length effects\faalkansen_rijntakken_update_excalibur.dtt"
     # projectfile = r"d:\TUD\P03 - Length effects\tst.json"
     # projectfile = r"d:\Documents\GitHub\anduryl\cases\CWD.dtt"
-    # projectfile = r"d:\Documents\GitHub\anduryl\ca ses\Arsenic D-R.dtt"
+    # projectfile = r"d:\Documents\GitHub\anduryl\cases\Arsenic D-R.dtt"
+    projectfile = r"d:\TUD\P11 - Metalog\allcases2\Arkansas.json"
     # projectfile = r"d:\Documents\GitHub\anduryl\cases\Arkansas.dtt"
-    projectfile = r"d:\TUD\P08 - Afvoerstatistiek EJ\Data\Elicitation\meuse_discharges_expertsessie.json"
+    # projectfile = r"d:\TUD\P08 - Afvoerstatistiek EJ\Data\Elicitation\meuse_discharges_expertsessie.json"
     # projectfile = r"d:\Documents\GitHub\anduryl\cases\ebbp.dtt"
     # projectfile = r"d:\Documents\GitHub\anduryl\cases\Biol agents.dtt"
     # projectfile = r"d:\Documents\GitHub\anduryl\cases\ATCEP error.dtt"
 
     ex.open_project(fname=projectfile)
+
+    # equal_settings = CalculationSettings(
+    #     id="EQ",
+    #     name='Equal',
+    #     weight='Equal',
+    #     overshoot=0.1,
+    #     alpha=0.0,
+    #     optimisation=False,
+    #     robustness=False,
+    #     calpower=1.0,
+    #     distribution=Distribution.PWL,
+    #     calibration_method=CalibrationMethod.LR
+    # )
+
+    # equal_settings2 = CalculationSettings(
+    #     id="EQ2",
+    #     name='Equal2',
+    #     weight='Equal',
+    #     overshoot=0.1,
+    #     alpha=0.0,
+    #     optimisation=False,
+    #     robustness=False,
+    #     calpower=1.0,
+    #     distribution=Distribution.METALOG,
+    #     calibration_method=CalibrationMethod.LR
+    # )
 
     # itemopt_settings = CalculationSettings(
     #     id="DM1",
@@ -52,22 +79,39 @@ if __name__ == "__main__":
     #     distribution="Metalog",
     # )
 
-    globalnonopt_settings = CalculationSettings(
-        id="DM1",
+    settings1 = CalculationSettings(
+        id="DM2",
         name="Global no opt.",
         weight="Global",
         overshoot=0.1,
         alpha=None, # TODO: For optimisation, alpha should be None. This is not checked
-        optimisation=True,
-        robustness=False,
+        optimisation=False,
+        robustness=True,
         calpower=1.0,
-        distribution="Metalog",
-        calibration_method=CalibrationMethod.CRPS
+        distribution=Distribution.METALOG,
+        calibration_method=CalibrationMethod.AD
     )
+
+
+    # globalnonopt_settings = CalculationSettings(
+    #     id="DM1",
+    #     name="Global no opt.",
+    #     weight="Global",
+    #     overshoot=0.1,
+    #     alpha=None, # TODO: For optimisation, alpha should be None. This is not checked
+    #     optimisation=True,
+    #     robustness=True,
+    #     calpower=1.0,
+    #     distribution=Distribution.METALOG,
+    #     calibration_method=CalibrationMethod.AD
+    # )
 
     # ex.project.experts.remove_expert("AR03")
 
+    ex.project.calculate_decision_maker(settings1)
     # ex.project.calculate_decision_maker(globalnonopt_settings)
+    # print()
+    # ex.project.calculate_decision_maker(equal_settings2)
 
     # ex.expertswidget.add_decision_maker()
 
@@ -86,5 +130,3 @@ if __name__ == "__main__":
     sys.exit(app.exec_())
 
 
-
-#TODO: CRPS 
--- debug_metalog_fitting.py
+++ debug_metalog_fitting.py
@@ -21,13 +21,18 @@ if __name__ == "__main__":
 
     itemopt_settings = CalculationSettings(
         id="DM1",
-        name="Item opt.",
-        weight="Item",
+        name="Global opt.",
+        weight="Global",
         overshoot=0.1,
         alpha=None,
-        robustness=False,
+        optimisation=True,
+        robustness=True,
         calpower=1.0,
         distribution="Metalog",
+        calibration_method='CRPS'
     )
 
     project.calculate_decision_maker(itemopt_settings)
+
+    project.calculate_item_robustness(itemopt_settings, max_exclude=1, min_exclude=0)
+        
